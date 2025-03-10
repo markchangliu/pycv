7ed3126c6e05a4a7406f869d19b7f9b873f8cb88
LIU Chang <liu67511717@126.com>
2025-03-10 10:33:49 +0800

    fix bugs

--- pycv/eval/det.py
+++ pycv/eval/det.py
@@ -8,7 +8,8 @@ import pycocotools.mask as pycocomask
 
 def assign_gt_to_dt(
     score_mat: np.ndarray,
-    thres: float
+    thres: float,
+    max_one_dt_per_gt: bool
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     为dt匹配gt，参考：
@@ -18,6 +19,9 @@ def assign_gt_to_dt(
     Args
     - `score_mat`: `Array[float]`, shape `(num_pred, num_gt)`
     - `thres`: `float`
+    - `max_one_dt_per_gt`: `bool`
+        - `True`: 一个gt最多匹配一个dt
+        - `False`: 一个gt可以匹配无数个dt
 
     Returns
     - `dt_gt_ids`: `Array[long]`, shape `(num_pred, )`,
@@ -26,11 +30,49 @@ def assign_gt_to_dt(
     dt最大分数是否超过`thres`
     - `gt_labels`: `Array[bool]`, shape `(num_gt, )`,
     gt是否有dt匹配
+
+    Examples
+    ```
+    score_mat = [
+        [0.4, 0.1, 0.3],
+        [0.3, 0.8, 0.2],
+        [0.1, 0.9, 0.4],
+        [0.1, 0.2, 0.7]
+    ]
+
+    # max_one_dt_per_gt为False, gt 1匹配了dt 1和dt 2
+    dt_gt_ids, dt_labels, gt_labels = assign_gt_to_dt(
+        score_mat, 0.5, False
+    )
+
+    print(dt_gt_ids)
+    >>> array([0, 1, 1, 2], dtype=int64)
+
+    print(dt_labels)
+    >>> array([False,  True,  True,  True])
+
+    print(gt_labels)
+    >>> array([False,  True,  True])
+
+    # max_one_dt_per_gt为True, gt 1只匹配了dt 1
+    dt_gt_ids, dt_labels, gt_labels = assign_gt_to_dt(
+        score_mat, 0.5, True
+    )
+
+    print(dt_gt_ids)
+    >>> array([0, 1, 1, 2], dtype=int64)
+
+    print(dt_labels)
+    >>> array([False,  False,  True,  True])
+
+    print(gt_labels)
+    >>> array([False,  True,  True])
+    ```
     """
     num_pred, num_gt = score_mat.shape
     score_mat = copy.deepcopy(score_mat)
 
-    dt_gt_ids = np.ones((num_pred, ), dtype=np.long) * -1
+    dt_gt_ids = np.ones((num_pred, ), dtype=np.int_) * -1
     dt_labels = np.zeros((num_pred, ), dtype=np.bool_)
     gt_labels = np.zeros((num_gt, ), dtype=np.bool_)
 
@@ -38,16 +80,19 @@ def assign_gt_to_dt(
         return dt_gt_ids, dt_labels, gt_labels
 
     # 为dt匹配分数最大的gt
-    max_scores_gt_ids = np.argmax(score_mat, axis=1)
-    max_scores_dt = score_mat[range(num_pred), max_scores_gt_ids]
+    dt_gt_ids = np.argmax(score_mat, axis=1)
+    max_scores_dt = score_mat[range(num_pred), dt_gt_ids]
 
     # 将分数大于thres的pred label设为True
-    dt_gt_ids = max_scores_gt_ids
     dt_labels[max_scores_dt>thres] = True
 
     # 将匹配到dt的gt label设为True
     max_scores_gt = np.max(score_mat, axis=0)
     gt_labels = max_scores_gt > thres
+
+    # 如果max_one_dt_per_gt为True, 将除max_score以外的dt_labels改为False
+    if max_one_dt_per_gt:
+        dt_labels[max_scores_dt<max_scores_gt[dt_gt_ids]] = False
     
     return dt_gt_ids, dt_labels, gt_labels
 
