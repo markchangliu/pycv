f16e60553167c305c794b964336d2ce75cd92724
markchangliu <liu67511717@126.com>
2025-04-03 13:04:38 +0800

    update data and datasets

--- pycv/data_structures/det_data.py
+++ pycv/data_structures/det_data.py
@@ -1,6 +1,6 @@
 import os
 from dataclasses import dataclass
-from typing import Union, List
+from typing import Union, List, Set
 
 import numpy as np
 
@@ -12,8 +12,8 @@ from pycv.data_structures.insts import Insts
 class DetData(BaseStructure):
     img: Union[np.ndarray, str, os.PathLike]
     insts: Insts
-    img_tags: List[str]
-    inst_tags: List[List[str]]
+    img_tags: List[Set[str]] # (num_img_tags, )
+    insts_tags: List[Set[str]] # (num_insts, (num_inst_tags, ))
 
     def __post_init__(self) -> None:
         self.validate()
@@ -29,5 +29,47 @@ class DetData(BaseStructure):
             if img_h != mask_img_h or img_w != mask_img_w:
                 raise ValueError("img and insts.mask have different sizes")
     
-    def get_data_of_inst_tags(inst_tags: List[str]) -> "DetData":
+    def get_insts_of_tags(
+        self,
+        target_insts_tags: List[str],
+        return_indice_flag: bool
+    ) -> "DetData":
+        new_insts_idx = []
+        target_insts_tags = set(target_insts_tags)
         
+        for inst_id, inst_tags in enumerate(self.insts_tags):
+            
+            if inst_tags.intersection(target_insts_tags):
+                new_insts_idx.append(inst_id)
+        
+        new_insts = self.insts[new_insts_idx]
+        new_insts_tags = [self.insts_tags[i] for i in new_insts_idx]
+
+        new_data  = DetData(
+            self.img, new_insts, self.img_tags, new_insts_tags
+        )
+
+        if return_indice_flag:
+            res = (new_data, new_insts_idx)
+        else:
+            res = new_data
+
+        return new_data
+            
+    def get_insts_of_cat_ids(
+        self,
+        target_cat_ids: List[int]
+    ) -> "DetData":
+        target_cat_ids = np.asarray(target_cat_ids)
+        inst_cat_ids = self.insts.cat_ids
+        
+        commons = np.intersect1d(
+            inst_cat_ids, target_cat_ids, return_indices=True
+        )
+
+        new_insts_ids = commons[1]
+        new_insts = self.insts[new_insts_ids]
+        new_insts_tags = [self.insts_tags[i] for i in new_insts_ids]
+        new_data = DetData(self.img, new_insts, self.img_tags, new_insts_tags)
+
+        return new_data
--- pycv/datasets/det_datasets.py
+++ pycv/datasets/det_datasets.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import List, Dict
+from typing import List, Dict, Set, Union
+
+import numpy as np
 
 from pycv.data_structures.det_data import DetData
 
@@ -10,23 +12,41 @@ class DetDataset:
     cat_id_name_dict: Dict[int, str] # (num_cats, )
     cat_name_id_dict: Dict[str, int] # (num_cats, )
     img_ids: List[int] # (num_imgs, )
-    inst_ids: List[List[int]] # (num_imgs, (num_insts_per_img, ))
+    insts_ids: List[List[int]] # (num_imgs, (num_insts_per_img, ))
 
     def __post_init__(self) -> None:
         self.validate()
+    
+    def __len__(self) -> int:
+        return len(self.data_list)
 
     def validate(self) -> None:
         assert len(self.data_list) == len(self.img_ids) \
-            == len(self.inst_ids)
+            == len(self.insts_ids)
         assert len(self.cat_id_name_dict) == len(self.cat_name_id_dict)
 
-    def get_data_of_cat_ids(self, cat_ids: List[int]) -> "DetDataset":
-        new_cat_id_name_dict = {}
-        new_cat_name_id_dict = {}
+    def get_data_of_cat_ids(
+        self, 
+        target_cat_ids: List[int]
+    ) -> "DetDataset":
+        new_cat_id_name_dict = {
+            k: v for k, v in self.cat_id_name_dict if k in target_cat_ids
+        }
+        new_cat_name_id_dict = {
+            k: v for k, v in self.cat_name_id_dict if v in target_cat_ids
+        }
 
-        for i, n in self.cat_id_name_dict.items():
-            if i in cat_ids:
-                new_cat_id_name_dict[i] = n
-                new_cat_name_id_dict[n] = i
+        new_data_list = []
+        new_img_ids = []
+        new_insts_ids = []
+
+        for img_id, inst_ids, data in zip(self.img_ids, self.insts_ids, self.data_list):
+            new_data = data.get_insts_of_cat_ids(target_cat_ids)
+            
+            if len(new_data) == 0:
+                continue
+
+            new_data_list.append(data)
         
+
         
