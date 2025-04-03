adee136fa9a40637bb866c07d4c18f6402203dba
markchangliu <liu67511717@126.com>
2025-04-03 18:09:33 +0800

    update det_data and det_dataset

--- pycv/data_structures/det_data.py
+++ pycv/data_structures/det_data.py
@@ -1,6 +1,7 @@
+import copy
 import os
 from dataclasses import dataclass
-from typing import Union, List, Set
+from typing import Union, List, Set, Tuple, Callable
 
 import numpy as np
 
@@ -12,7 +13,7 @@ from pycv.data_structures.insts import Insts
 class DetData(BaseStructure):
     img: Union[np.ndarray, str, os.PathLike]
     insts: Insts
-    img_tags: List[Set[str]] # (num_img_tags, )
+    img_tags: Set[str] # (num_img_tags, )
     insts_tags: List[Set[str]] # (num_insts, (num_inst_tags, ))
 
     def __post_init__(self) -> None:
@@ -20,6 +21,22 @@ class DetData(BaseStructure):
     
     def __len__(self) -> int:
         return len(self.insts)
+    
+    def __getitem__(
+        self, 
+        item: Union[int, List[int], slice, np.ndarray]
+    ) -> "DetData":
+        if isinstance(item, int):
+            item = [item]
+        if isinstance(item, slice):
+            item = list(range(len(self.insts))[item])
+        
+        new_insts = self.insts[item]
+        new_insts_tags = [self.insts_tags[i] for i in item]
+        new_data = DetData(self.img, new_insts, new_insts_tags)
+
+        return new_data
+
 
     def validate(self):
         if isinstance(self.img, str) and self.insts.masks is not None:
@@ -31,26 +48,26 @@ class DetData(BaseStructure):
     
     def get_insts_of_tags(
         self,
-        target_insts_tags: List[str],
-        return_indice_flag: bool
-    ) -> "DetData":
-        new_insts_idx = []
-        target_insts_tags = set(target_insts_tags)
+        target_inst_tags: List[str],
+        return_idx_flag: bool
+    ) -> Union["DetData", Tuple["DetData", List[int]]]:
+        new_inst_ids = []
+        target_inst_tags = set(target_inst_tags)
         
         for inst_id, inst_tags in enumerate(self.insts_tags):
             
-            if inst_tags.intersection(target_insts_tags):
-                new_insts_idx.append(inst_id)
+            if inst_tags.intersection(target_inst_tags):
+                new_inst_ids.append(inst_id)
         
-        new_insts = self.insts[new_insts_idx]
-        new_insts_tags = [self.insts_tags[i] for i in new_insts_idx]
+        new_insts = self.insts[new_inst_ids]
+        new_inst_tags = [self.insts_tags[i] for i in new_inst_ids]
 
         new_data  = DetData(
-            self.img, new_insts, self.img_tags, new_insts_tags
+            self.img, new_insts, self.img_tags, new_inst_tags
         )
 
-        if return_indice_flag:
-            res = (new_data, new_insts_idx)
+        if return_idx_flag:
+            res = (new_data, new_inst_ids)
         else:
             res = new_data
 
@@ -58,8 +75,9 @@ class DetData(BaseStructure):
             
     def get_insts_of_cat_ids(
         self,
-        target_cat_ids: List[int]
-    ) -> "DetData":
+        target_cat_ids: List[int],
+        return_idx_flag: bool
+    ) -> Union["DetData", Tuple["DetData", List[int]]]:
         target_cat_ids = np.asarray(target_cat_ids)
         inst_cat_ids = self.insts.cat_ids
         
@@ -67,9 +85,45 @@ class DetData(BaseStructure):
             inst_cat_ids, target_cat_ids, return_indices=True
         )
 
-        new_insts_ids = commons[1]
-        new_insts = self.insts[new_insts_ids]
-        new_insts_tags = [self.insts_tags[i] for i in new_insts_ids]
-        new_data = DetData(self.img, new_insts, self.img_tags, new_insts_tags)
+        new_inst_ids = commons[1]
+        new_insts = self.insts[new_inst_ids]
+        new_inst_tags = [self.insts_tags[i] for i in new_inst_ids]
+        new_data = DetData(self.img, new_insts, self.img_tags, new_inst_tags)
 
-        return new_data
+        if return_idx_flag:
+            res = (new_data, new_inst_ids)
+        else:
+            res = new_data
+
+        return res
+    
+    def tag_img(
+        self,
+        new_tags: List[str],
+        lambda_funcs: List[Callable[[Union[str, os.PathLike, np.ndarray]], bool]],
+        retag_flag: bool
+    ) -> None:
+        img_tags = self.img_tags
+        new_img_tags = [] if retag_flag else copy.deepcopy(img_tags)
+
+        for new_tag, lambda_func in zip(new_tags, lambda_funcs):
+            if lambda_func(self.img):
+                new_img_tags.append(new_tag)
+        
+        self.img_tags = set(new_img_tags)
+    
+    def tag_insts(
+        self,
+        new_tags: List[str],
+        lambda_funcs: List[Callable[[Insts], bool]],
+        retag_flag: bool
+    ) -> None:
+        insts_tags = [list(ts) for ts in self.insts_tags]
+        new_insts_tags = [[] * len(self.insts)] if retag_flag else copy.deepcopy(insts_tags)
+
+        for new_tag, lambda_func in zip(new_tags, lambda_funcs):
+            for inst_id, inst in enumerate(self.insts):
+                if lambda_func(inst):
+                    new_insts_tags[inst_id].append(new_tag)
+
+        self.insts_tags = [set(ts) for ts in new_insts_tags]
--- pycv/datasets/det_datasets.py
+++ pycv/datasets/det_datasets.py
@@ -1,8 +1,10 @@
+import os
 from dataclasses import dataclass
-from typing import List, Dict, Set, Union
+from typing import List, Dict, Union, Callable
 
 import numpy as np
 
+from pycv.data_structures.insts import Insts
 from pycv.data_structures.det_data import DetData
 
 
@@ -19,6 +21,30 @@ class DetDataset:
     
     def __len__(self) -> int:
         return len(self.data_list)
+    
+    def __getitem__(
+        self, 
+        item: Union[int, List[int], slice, np.ndarray]
+    ) -> "DetDataset":
+        if isinstance(item, int):
+            item = [item]
+        elif isinstance(item, slice):
+            item = list(range(len(self.insts))[item])
+        elif isinstance(item, np.ndarray):
+            if item.dtype == np.bool_:
+                item = np.nonzero(item)[0].tolist()
+            else:
+                item = item.tolist()
+        
+        new_data_list = [self.data_list[i] for i in item]
+        new_img_ids = item
+        new_insts_ids = [self.insts_ids[i] for i in item]
+        new_dataset = DetDataset(
+            new_data_list, self.cat_id_name_dict, self.cat_name_id_dict,
+            new_img_ids, new_insts_ids
+        )
+
+        return new_dataset
 
     def validate(self) -> None:
         assert len(self.data_list) == len(self.img_ids) \
@@ -40,13 +66,87 @@ class DetDataset:
         new_img_ids = []
         new_insts_ids = []
 
-        for img_id, inst_ids, data in zip(self.img_ids, self.insts_ids, self.data_list):
-            new_data = data.get_insts_of_cat_ids(target_cat_ids)
+        for img_id, data in zip(self.img_ids, self.data_list):
+            new_data, new_inst_ids = data.get_insts_of_cat_ids(target_cat_ids, True)
             
-            if len(new_data) == 0:
+            if len(new_inst_ids) == 0:
                 continue
 
             new_data_list.append(data)
+            new_img_ids.append(img_id)
+            new_insts_ids.append(new_inst_ids)
         
+        new_dataset = DetDataset(
+            new_data_list, new_cat_id_name_dict, new_cat_name_id_dict,
+            new_img_ids, new_insts_ids
+        )
 
+        return new_dataset
+    
+    def get_data_of_img_tags(
+        self,
+        target_img_tags: List[str]
+    ) -> "DetDataset":
+        target_img_tags = set(target_img_tags)
+        new_data_list = []
+        new_img_ids = []
+        new_insts_ids = []
+
+        for img_id, inst_ids, data in zip(self.img_ids, self.insts_ids, self.data_list):
+            if data.img_tags.intersection(target_img_tags):
+                new_data_list.append(data)
+                new_img_ids.append(img_id)
+                new_insts_ids.append(inst_ids)
+        
+        new_dataset = DetDataset(
+            new_data_list, self.cat_id_name_dict, self.cat_name_id_dict,
+            new_img_ids, new_insts_ids
+        )
+
+        return new_dataset
+    
+    def get_data_of_inst_tags(
+        self,
+        target_inst_tags: List[str],
+    ) -> "DetDataset":
+        target_inst_tags = set(target_inst_tags)
+        new_data_list = []
+        new_img_ids = []
+        new_insts_ids = []
+
+        for img_id, inst_ids, data in zip(self.img_ids, self.insts_ids, self.data_list):
+            new_data, new_inst_ids_local = data.get_insts_of_tags(target_inst_tags, True)
+            new_inst_ids = inst_ids[new_inst_ids_local]
+
+            if len(new_inst_ids_local) == 0:
+                continue
+
+            new_data_list.append(new_data)
+            new_img_ids.append(img_id)
+            new_insts_ids.append(new_inst_ids)
+            
+        new_dataset = DetDataset(
+            new_data_list, self.cat_id_name_dict, self.cat_name_id_dict,
+            new_img_ids, new_insts_ids
+        )
+
+        return new_dataset
+    
+    def tag_imgs(
+        self,
+        new_tags: List[str],
+        lambda_funcs: List[Callable[[Union[str, os.PathLike, np.ndarray]], bool]],
+        retag_flag: bool
+    ) -> None:
+        for data in self.data_list:
+            data.tag_img(new_tags, lambda_funcs, retag_flag)
+    
+    def tag_insts(
+        self,
+        new_tags: List[str],
+        lambda_funcs: List[Callable[[Insts], bool]],
+        retag_flag: bool
+    ) -> None:
+        for data in self.data_list:
+            data.tag_insts(new_tags, lambda_funcs, retag_flag)
         
