9ecae673d63689e180c706dd7dcc63a29e97fc77
LIU Chang <liu67511717@126.com>
2025-04-07 08:48:28 +0000

    update load_dataset_from_coco_json

--- pycv/datasets/loaders.py
+++ pycv/datasets/loaders.py
@@ -84,23 +84,76 @@ def load_dataset_from_coco_json(
         cat_name_id_dict[cat_name] = cat_id
         cat_id_name_dict[cat_id] = cat_name
     
-    imgs = []
+    img_ps = []
     img_ids = []
+    img_hws = []
+    imgs_tags = []
     
     for img_info in coco_dict["images"]:
         img_p = os.path.join(img_prefix, img_info["file_name"])
         img_id = img_info["id"]
-        imgs.append(img_p)
+        img_hw = (img_info["height"], img_info["width"])
+        img_tags = img_info.get("tags", [])
+        img_ps.append(img_p)
         img_ids.append(img_id)
+        img_hws.append(img_hw)
+        imgs_tags.append(img_tags)
     
-    anns = []
-    ann_ids = [[] * (len(img_ids) + 1)]
+    imgs_bboxes = [[] * max(img_ids)]
+    imgs_masks = [[] * max(img_ids)]
+    imgs_cat_ids = [[] * max(img_ids)]
+    imgs_confs = [[] * max(img_ids)]
+    imgs_insts_tags = [[] * max(img_ids)]
+    imgs_insts_ids = [[] * max(img_ids)]
+
     for ann_info in coco_dict["annotations"]:
         conf = 1
         cat_id = ann_info["category_id"]
         bbox = ann_info["bbox"]
-        segm = ann_info["segmentation"]
+        segm = ann_info.get(["segmentation"], None)
         ann_img_id = ann_info["image_id"]
+        inst_tags = ann_info.get("tags", [])
+        ann_id = ann_info["id"]
+
+        imgs_masks[ann_img_id].append(segm)
+        imgs_bboxes[ann_img_id].append(bbox)
+        imgs_cat_ids[ann_img_id].append(cat_id)
+        imgs_confs[ann_img_id].append(conf)
+        imgs_insts_tags[ann_img_id].append(inst_tags)
+        imgs_insts_ids[ann_img_id].append(ann_id)
+    
+    data_list = []
+
+    for i in range(len(img_ids)):
+        img_id = img_ids[i]
+        img_p = img_ps[i]
+        img_hw = img_hws[i]
+        img_tags = imgs_tags[i]
+
+        img_bboxes = imgs_bboxes[img_id]
+        img_masks = imgs_masks[img_id] if imgs_masks[img_id][0] else None
+        img_cat_ids = imgs_cat_ids[img_id]
+        img_confs = imgs_confs[img_id]
+        img_insts_tags = imgs_insts_tags[img_id]
+
+        img_bboxes = BBoxes(img_bboxes, BBoxFormat.XYWH)
+        img_masks = Masks(img_masks, img_hw, MaskFormat.POLY) if img_masks else None
+        img_confs = np.asarray(img_confs)
+        img_cat_ids = np.asarray(img_cat_ids)
+        img_tags = set(img_tags)
+        insts_tags = [set(ts) for ts in img_insts_tags]
+        insts = Insts(img_confs, img_cat_ids, img_bboxes, img_masks)
+        img_data = DetData(img_p, insts, img_tags, insts_tags)
+
+        data_list.append(img_data)
+    
+    img_dataset = DetDataset(
+        data_list, cat_id_name_dict, cat_name_id_dict,
+        img_ids, imgs_insts_ids
+    )
+
+    return img_dataset
+
 
         
     
